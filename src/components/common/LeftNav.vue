9779569c956ff188a71bc611b9f08acb1ddc4dd7
franklioxygen <frank.li.oxygen@gmail.com>
2022-03-12 21:27:07 -0500

    toggle theme

--- src/components/common/LeftNav.vue
+++ src/components/common/LeftNav.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="nav-container">
+    <input
+      type="checkbox"
+      name="mode"
+      id="change-theme"
+      :checked="isDarkMode()"
+    />
+    <span> Toggle Theme</span>
     <router-link
       v-for="(route, id) in routes"
       :to="route.path"
@@ -7,21 +14,39 @@
       @click="changeRoute"
       >{{ route.name }}</router-link
     >
-    test <br />test <br />test <br />test <br />test <br />test <br />test
-    <br />test <br />test <br />test <br />test <br />test <br />test <br />test
-    <br />test <br />test <br />test <br />test <br />test <br />test <br />test
-    <br />test <br />test <br />test <br />test <br />test <br />test <br />test
-    <br />test <br />test <br />test <br />
   </div>
 </template>
 
 <script setup>
+import { onMounted } from "@vue/runtime-core";
 import routes from "../../router/routes";
 const emit = defineEmits(["change-route"]);
 const changeRoute = () => emit("change-route");
-// let changeRoute = () => {
-//   emit(["change-route"]);
-// };
+let setDarkMode = () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+};
+let setLightMode = () => {
+  document.documentElement.setAttribute("data-theme", "light");
+};
+let isDarkMode = () => {
+  return (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+};
+onMounted(() => {
+  const changeTheme = document.getElementById("change-theme");
+  changeTheme.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      setDarkMode();
+    } else {
+      setLightMode();
+    }
+  });
+  if (isDarkMode()) {
+    setDarkMode();
+  }
+});
 </script>
 
 <style lang="scss" scoped>
