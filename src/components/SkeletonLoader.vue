40426d3681efee825a62c5d34935447dd0ebf1db
Peifan Li <franklioxygen@frankMBA.local>
2022-03-16 12:54:54 -0400

    skeleton loader

---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="skeleton-wrapper" :style="skeletonStyle" />
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "SearchBar",
+  props: {
+    width: {
+      type: String,
+      default: "100%",
+    },
+    height: {
+      type: String,
+      default: "1rem",
+    },
+    borderRadius: {
+      type: String,
+      default: "0.25rem",
+    },
+  },
+  setup(props) {
+    const skeletonStyle = computed(() => {
+      return {
+        width: props.width,
+        height: props.height,
+        borderRadius: props.borderRadius,
+      };
+    });
+    return { skeletonStyle };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@keyframes fade {
+  60% {
+    opacity: 0.6;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.skeleton-wrapper {
+  animation: fade 1.5s alternate infinite;
+  background-color: var(--color-skeleton-loader);
+}
+</style>
---
+++ src/demo/SkeletonLoaderDemo.vue
@@ -0,0 +1,61 @@
+<template>
+  <h3>Skeleton Loader</h3>
+  <p>Skeleton Loader is used as placeholder on data loading.</p>
+  <h5>Demo:</h5>
+  <div class="demo-wrapper">
+    <SkeletonLoader />
+    <SkeletonLoader height="4rem" />
+    <SkeletonLoader width="200px" height="2rem" />
+    <SkeletonLoader width="50px" height="50px" borderRadius="50%" />
+    <SkeletonLoader
+      v-for="(n, index) of 4"
+      v-bind:key="index"
+      width="60px"
+      height="2rem"
+    />
+  </div>
+  <h5>Usage:</h5>
+  <h6>Insert component:</h6>
+  <highlightjs language="html" :code="insertCode" />
+  <h6>Import:</h6>
+  <highlightjs language="js" :code="importCode" />
+</template>
+
+<script>
+import { computed, reactive } from "vue";
+import SkeletonLoader from "../components/SkeletonLoader.vue";
+export default {
+  name: "DropdownSelectDemo",
+  components: { SkeletonLoader },
+  setup() {
+    let searchEventReturnValue = reactive({
+      returnValue: "keyword",
+    });
+    const insertCode = computed(() => {
+      return `
+<SkeletonLoader width="50px" height="50px" borderRadius="50%" />
+      `;
+    });
+    const importCode = computed(() => {
+      return `
+import SkeletonLoader from "../components/SkeletonLoader.vue";
+      `;
+    });
+
+    return {
+      searchEventReturnValue,
+      insertCode,
+      importCode,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.demo-wrapper {
+  > * {
+    display: inline-block;
+    margin-right: 50px;
+  }
+}
+</style>
