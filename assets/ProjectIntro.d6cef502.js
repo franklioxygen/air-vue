c92bb95d0d935448aae8bb64e15dd8ae4c41a0d9
Peifan Li <franklioxygen@frankMBA.local>
2022-03-16 15:37:04 -0400

    deploy

--- assets/ProjectIntro.d6cef502.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.f7df140e.js";import{o as t,c as n,F as s,a as o}from"./vendor.75353d22.js";const r={},c=o("h3",null,"Introduction",-1),a=o("p",null,"VU is a set of open source components library for Vue3",-1),_=o("h5",null,"Setup",-1),l=o("p",null,"TBD",-1);function p(u,i,d,f,m,h){return t(),n(s,null,[c,a,_,l],64)}var x=e(r,[["render",p]]);export{x as default};
