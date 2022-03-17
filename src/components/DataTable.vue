92d1c01b98b9c5612dbfe250a9ba02ef39ffb651
Peifan Li <franklioxygen@frankMBA.local>
2022-03-16 23:07:02 -0400

    datatable

---
+++ src/components/DataTable.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="table-wrapper" :style="tableStyle">
+    <span class="table-title">{{ tableProps.title }}</span>
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="(th, index) in tableProps.tableHead" :key="index">
+            {{ th.label }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(tr, index) in tableProps.tableBody" :key="index">
+          <td v-for="(td, index) in tableProps.tableHead" :key="index">
+            {{ tr[td.field] }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+export default {
+  name: "DataTable",
+  props: {
+    width: {
+      type: String,
+      default: "100%",
+    },
+    tableProps: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const tableStyle = computed(() => {
+      return props.width !== null ? `width: ${props.width}` : "";
+    });
+    return {
+      tableStyle,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.table-wrapper {
+  .table-title {
+    font-size: 1.2rem;
+    // font-weight: bold;
+    text-align: center;
+    padding: 0.5rem 0;
+  }
+  .table {
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    th {
+      font-size: 1rem;
+      text-align: center;
+      font-weight: 200;
+      padding: 0.5rem 0;
+      color: var(--color-heading);
+      background-color: var(--color-background-mute);
+    }
+    td {
+      font-size: 0.8rem;
+      text-align: center;
+      padding: 0.5rem 0;
+      color: var(--color-text);
+      background-color: var(--color-background-soft);
+    }
+  }
+}
+</style>
---
+++ src/demo/DataTableDemo.vue
@@ -0,0 +1,89 @@
+<template>
+  <h3>Data Table</h3>
+  <p>Data Table is used to display data in a table.</p>
+  <h5>Demo:</h5>
+  <div class="demo-wrapper">
+    <DataTable :table-props="tableProps" />
+  </div>
+  <h5>Usage:</h5>
+  <h6>Insert component:</h6>
+  <highlightjs language="html" :code="insertCode" />
+  <h6>Import:</h6>
+  <highlightjs language="js" :code="importCode" />
+  <h6>Apply in template：</h6>
+  <highlightjs language="js" :code="templateCode" />
+</template>
+
+<script>
+import { computed } from "vue";
+import DataTable from "../components/DataTable.vue";
+export default {
+  name: "DataTableDemo",
+  components: { DataTable },
+  setup() {
+    const insertCode = computed(() => {
+      return `
+
+      `;
+    });
+    const importCode = computed(() => {
+      return `
+import DataTable from "../components/DataTable.vue";
+      `;
+    });
+    const templateCode = computed(() => {
+      return `
+      `;
+    });
+    const tableProps = computed(() => {
+      return {
+        title: "Demo Data Table Title", //optional
+        tableHead: [
+          {
+            label: "Product Name",
+            field: "name",
+          },
+          {
+            label: "Price($)",
+            field: "price",
+          },
+          {
+            label: "Weight(kg)",
+            field: "weight",
+          },
+          {
+            label: "Dimensions(w x h x d)",
+            field: "dimensions",
+          },
+        ],
+        tableBody: [
+          { name: "Apple", price: "1.00", weight: "1.23", dimensions: "3x2x3" },
+          { name: "Pear", price: "2.00", weight: "2.10", dimensions: "4x3x3" },
+          {
+            name: "Banana",
+            price: "0.50",
+            weight: "0.76",
+            dimensions: "3x8x3",
+          },
+        ],
+      };
+    });
+
+    return {
+      insertCode,
+      importCode,
+      templateCode,
+      tableProps,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.demo-wrapper {
+  > * {
+    display: inline-block;
+    margin-right: 50px;
+  }
+}
+</style>
