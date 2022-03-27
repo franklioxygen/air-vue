96ca301b84c8aea3e862f91a77acf6a67a73ea01
Peifan Li <franklioxygen@frankMBA.local>
2022-03-26 23:54:25 -0400

    deploy

--- assets/ToggleButtonDemo.1a1f66cc.js
+++ assets/ToggleButtonDemo.1a1f66cc.js
@@ -1,4 +1,4 @@
-import{_,T as p}from"./index.7b3dd353.js";import{s as n,r as d,f as i,o as h,c as m,a as t,g as l,t as v,F as B}from"./vendor.ebe7cf86.js";const T={name:"ToggleButtonDemo",components:{ToggleButton:p},setup(){const s=n(()=>`
+import{_,T as p}from"./index.9a542061.js";import{s as n,r as d,f as i,o as h,c as m,a as t,g as l,t as v,F as B}from"./vendor.ebe7cf86.js";const T={name:"ToggleButtonDemo",components:{ToggleButton:p},setup(){const s=n(()=>`
 <ToggleButton
   id="demo-toggle-button"
   :checked="checked.value"
