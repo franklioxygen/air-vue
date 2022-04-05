8aa0f955789809a290c704855b1bca6b8be36757
franklioxygen <frank.li.oxygen@gmail.com>
2022-04-05 14:34:52 -0400

    adjust filename

---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="toggle-wrapper">
+    <input type="checkbox" :id="id" :checked="checked" />
+    <div class="switch-box" @click="toggleState()">
+      <div class="switch-bar"></div>
+    </div>
+    <div class="toggle-label" v-if="toggleProps">
+      {{ toggleProps.label }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, reactive } from "@vue/runtime-core";
+export default {
+  name: "ToggleSwitch",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    checked: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    toggleProps: {
+      type: Object,
+      required: false,
+    },
+  },
+  emits: ["toggle-switch"],
+  setup(props, { emit }) {
+    let currentState = reactive({
+      state: props.checked,
+    });
+    const toggleState = () => {
+      currentState.state = !currentState.state;
+      toggleButtonEmit(currentState.state);
+      moveBar();
+    };
+    const toggleButtonEmit = (state) => {
+      emit("toggle-switch", state);
+    };
+    const moveBar = () => {
+      const targetBox = (event.path || event.composedPath()).filter((e) =>
+        e.classList ? e.classList.contains("switch-box") : ""
+      )[0].classList;
+      targetBox.toggle("active");
+      document.getElementById(props.id).checked = !document.getElementById(
+        props.id
+      ).checked;
+    };
+    onMounted(() => {
+      currentState.state === true ? moveBar() : null;
+    });
+    return { currentState, toggleState, toggleButtonEmit };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.toggle-wrapper {
+  input {
+    transform: scale(0);
+    position: absolute;
+  }
+  .switch-box {
+    display: inline-block;
+    width: 60px;
+    background: var(--color-background-highlight);
+    padding: 0 !important;
+    border: 0.1rem solid var(--color-border-hover);
+    transition: 0.5s;
+    cursor: pointer;
+    &.active {
+      background: $green;
+      .switch-bar {
+        position: absolute;
+        left: 28px;
+      }
+    }
+    .switch-bar {
+      transition: 0.3s;
+      left: 0;
+      border-radius: 0.3rem;
+      border: 0.1rem solid var(--color-border);
+      width: 29px;
+      height: 29px;
+      background: var(--color-background-mute);
+    }
+  }
+  .toggle-label {
+    display: inline-block;
+    margin-left: 20px;
+  }
+}
+</style>
---
+++ src/demo/ToggleSwitchDemo.vue
@@ -0,0 +1,100 @@
+<template>
+  <h3>Toggle Button</h3>
+  <p>Toggle Button is used to toggle.</p>
+  <h5>Demo:</h5>
+  <div class="demo-wrapper">
+    <ToggleSwitch
+      id="demo-toggle-switch"
+      :checked="checked.value"
+      :toggle-props="toggleProps"
+      @toggle-switch="toggleButtonEvent"
+    />
+    <div>
+      This is the value return from Toggle:
+      {{ toggleEventReturnValue.returnValue }}
+    </div>
+  </div>
+  <h5>Usage:</h5>
+  <h6>Insert component:</h6>
+  <highlightjs language="html" :code="insertCode" />
+  <h6>Import:</h6>
+  <highlightjs language="js" :code="importCode" />
+  <h6>Apply in template：</h6>
+  <highlightjs language="js" :code="templateCode" />
+</template>
+
+<script>
+import { computed, reactive } from "vue";
+import ToggleSwitch from "../components/ToggleSwitch.vue";
+
+export default {
+  name: "ToggleButtonDemo",
+  components: { ToggleSwitch },
+  setup() {
+    const insertCode = computed(() => {
+      return `
+<ToggleButton
+  id="demo-toggle-switch"
+  :checked="checked.value"
+  :toggle-props="toggleProps"
+  @toggle-switch="toggleButtonEvent"
+/>
+      `;
+    });
+    const importCode = computed(() => {
+      return `
+import ToggleButton from "../components/ToggleButton.vue";
+      `;
+    });
+    const templateCode = computed(() => {
+      return `
+const checked = reactive({
+    value: false,
+});
+const toggleProps = computed(() => {
+    return {
+        label: "Toggle Button",
+    };
+});
+const toggleButtonEvent = (event) => {
+    // Do something with the event
+    toggleEventReturnValue.returnValue = event;
+};
+      `;
+    });
+    const checked = reactive({
+      value: false,
+    });
+    let toggleEventReturnValue = reactive({
+      returnValue: checked.value,
+    });
+    const toggleProps = computed(() => {
+      return {
+        label: "Toggle Button",
+      };
+    });
+    const toggleButtonEvent = (event) => {
+      // Do something with the event
+      toggleEventReturnValue.returnValue = event;
+    };
+    return {
+      toggleEventReturnValue,
+      checked,
+      toggleProps,
+      insertCode,
+      importCode,
+      templateCode,
+      toggleButtonEvent,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.demo-wrapper {
+  > * {
+    display: inline-block;
+    margin-right: 50px;
+  }
+}
+</style>
