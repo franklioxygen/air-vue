59ec0dbc5f75b52148a29fb353d206f9d94464ef
franklioxygen <frank.li.oxygen@gmail.com>
2022-04-05 22:20:42 -0400

    inline message

---
+++ src/components/InlineMessage.vue
@@ -0,0 +1,149 @@
+<template>
+  <div :class="['inline-msg-wrapper', messageProps.status]">
+    <div :class="['icons-msg', 'icons', type]"></div>
+    <div :class="['inline-msg-content', messageProps.status]">
+      <div :class="['msg-type-name', type]">{{ type }}</div>
+      {{ messageProps.content }}
+    </div>
+    <div
+      :class="['icon-close icons', messageProps.status]"
+      @click="closeMsg()"
+    ></div>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  name: "InlineMessage",
+  props: {
+    expanded: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: "info",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["close-msg"],
+  setup(props) {
+    const messageProps = reactive({
+      status: props.expanded ? "expanded" : "collapsed",
+      content: props.content,
+    });
+    const openMsg = () => {
+      new Promise((resolve) => {
+        closeMsg();
+        resolve();
+      }).then(() => {
+        new Promise((resolve) => {
+          setTimeout(() => {
+            messageProps.status = "expanded";
+            messageProps.content = props.content;
+            resolve();
+          }, 300);
+        });
+      });
+    };
+    const closeMsg = () => {
+      messageProps.content = "";
+      messageProps.status = "collapsed";
+    };
+    return {
+      messageProps,
+      openMsg,
+      closeMsg,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.inline-msg-wrapper {
+  position: relative;
+  background-color: var(--color-background-soft);
+  border: 1px solid var(--color-border);
+  width: 100%;
+  border-radius: var(--input-radius);
+  padding: 10px;
+  color: var(--color-text);
+  margin-bottom: 10px;
+  &.expanded {
+    transition: max-height 0.3s ease-in-out;
+    overflow: hidden;
+    max-height: 1000px;
+  }
+  &.collapsed {
+    transition: 0.3s;
+    max-height: 0;
+    padding: 0;
+    margin: 0;
+    border: 0;
+    opacity: 0;
+  }
+
+  .icons-msg {
+    display: inline-block;
+    vertical-align: middle;
+    margin-right: 7px;
+    cursor: auto;
+    top: 2px;
+    vertical-align: top;
+    &.success {
+      background: url("../assets/icons/LinearIconsSVG/checkmark-circle.svg");
+    }
+    &.warning {
+      background: url("../assets/icons/LinearIconsSVG/warning.svg");
+    }
+    &.failed {
+      background: url("../assets/icons/LinearIconsSVG/cross-circle.svg");
+    }
+    &.info {
+      background: url("../assets/icons/LinearIconsSVG/bullhorn.svg");
+    }
+  }
+  .icon-close {
+    display: inline-block;
+    background: url("../assets/icons/LinearIconsSVG/cross.svg");
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    right: 10px;
+    &.collapsed {
+      display: none;
+    }
+  }
+  .msg-type-name {
+    display: inline-block;
+    margin-right: 5px;
+
+    &.success {
+      color: rgb(0, 114, 0);
+    }
+    &.warning {
+      color: rgb(196, 196, 19);
+    }
+    &.failed {
+      color: rgb(191, 0, 0);
+    }
+    &.info {
+      color: rgb(21, 109, 197);
+    }
+  }
+  .msg-type-name::first-letter {
+    text-transform: capitalize;
+  }
+  .inline-msg-content {
+    display: inline-block;
+    width: calc(100% - 50px);
+    &.collapsed {
+      display: none;
+    }
+  }
+}
+</style>
---
+++ src/demo/InlineMessageDemo.vue
@@ -0,0 +1,139 @@
+<template>
+  <h3>Inline Message</h3>
+  <p>Inline Message is to display message to user.</p>
+  <h5>Demo:</h5>
+  <div class="demo-wrapper">
+    <CustomizedButton
+      button-text="Success Message"
+      width="200px"
+      level="vu-btn-primary"
+      @click-button="popSuccessMessage()"
+    />
+    <CustomizedButton
+      button-text="Warning Message"
+      width="200px"
+      level="vu-btn-primary"
+      @click-button="popWarningMessage()"
+    />
+    <CustomizedButton
+      button-text="Failed Message"
+      width="200px"
+      level="vu-btn-primary"
+      @click-button="popFailedMessage()"
+    />
+    <CustomizedButton
+      button-text="Info Message"
+      width="200px"
+      level="vu-btn-primary"
+      @click-button="popInfoMessage()"
+    />
+    <InlineMessage
+      :type="messageProps.type"
+      :content="messageProps.content"
+      :expanded="messageProps.expanded"
+      ref="exampleInlineMessage"
+    />
+  </div>
+  <h5>Usage:</h5>
+  <h6>Insert component:</h6>
+  <highlightjs language="html" :code="insertCode" />
+  <h6>Import:</h6>
+  <highlightjs language="js" :code="importCode" />
+  <h6>Apply in template：</h6>
+  <highlightjs language="js" :code="templateCode" />
+</template>
+
+<script>
+import { computed, reactive, ref } from "vue";
+import InlineMessage from "../components/InlineMessage.vue";
+import CustomizedButton from "../components/CustomizedButton.vue";
+export default {
+  name: "InlineMessageDemo",
+  components: { InlineMessage, CustomizedButton },
+  setup() {
+    let messageProps = reactive({
+      type: "",
+      content: "",
+      expanded: false,
+    });
+    const exampleInlineMessage = ref();
+    const popSuccessMessage = () => {
+      exampleInlineMessage.value.openMsg();
+      messageProps.type = "success";
+      messageProps.content = `
+        This is a Success Message. Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Vestibulum eu pharetra turpis, 
+        ut congue ipsum. Nunc tincidunt ipsum ullamcorper ornare 
+        fringilla. Mauris et accumsan elit, at lacinia erat. 
+        Phasellus id interdum tortor. Morbi ac nunc dolor. 
+        Vivamus consequat eleifend nulla in aliquam. Proin 
+        facilisis faucibus sem sit amet placerat.`;
+    };
+    const popWarningMessage = () => {
+      exampleInlineMessage.value.openMsg();
+      messageProps.type = "warning";
+      messageProps.content = "This is a Warning Message.";
+    };
+    const popFailedMessage = () => {
+      exampleInlineMessage.value.openMsg();
+      messageProps.type = "failed";
+      messageProps.content = "This is a Failed Message.";
+    };
+    const popInfoMessage = () => {
+      exampleInlineMessage.value.openMsg();
+      messageProps.type = "info";
+      messageProps.content = "This is a Info Message.";
+    };
+    const insertCode = computed(() => {
+      return `
+<InlineMessage
+    :type="messageProps.type"
+    :content="messageProps.content"
+    :expanded="messageProps.expanded"
+    ref="exampleInlineMessage"
+/>
+      `;
+    });
+    const importCode = computed(() => {
+      return `
+import InlineMessage from "../components/InlineMessage.vue";
+      `;
+    });
+    const templateCode = computed(() => {
+      return `
+let messageProps = reactive({
+    type: "",
+    content: "",
+    expanded: false,
+});
+const exampleInlineMessage = ref();
+const popSuccessMessage = () => {
+    exampleInlineMessage.value.openMsg();
+    messageProps.type = "success";
+    messageProps.content = "This is a Success Message.";
+};
+      `;
+    });
+    return {
+      messageProps,
+      insertCode,
+      importCode,
+      templateCode,
+      exampleInlineMessage,
+      popSuccessMessage,
+      popWarningMessage,
+      popFailedMessage,
+      popInfoMessage,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.demo-wrapper {
+  > * {
+    display: inline-block;
+    margin-right: 50px;
+  }
+}
+</style>
