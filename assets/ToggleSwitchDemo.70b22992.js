cb2c85adb1f4b115c29492bd80572e2d61c2639f
franklioxygen <frank.li.oxygen@gmail.com>
2022-04-16 21:58:10 -0400

    deploy

--- assets/ToggleSwitchDemo.70b22992.js
+++ assets/ToggleSwitchDemo.70b22992.js
@@ -1,4 +1,4 @@
-import{_ as d,T as _}from"./index.d472e248.js";import{s as l,r as h,f as u,o as p,c as m,a as t,g as n,t as v,F as w}from"./vendor.54853348.js";const S={name:"ToggleSwitchDemo",components:{ToggleSwitch:_},setup(){const c=l(()=>`
+import{_ as d,T as _}from"./index.e94bc48f.js";import{s as l,r as h,f as u,o as p,c as m,a as t,g as n,t as v,F as w}from"./vendor.54853348.js";const S={name:"ToggleSwitchDemo",components:{ToggleSwitch:_},setup(){const c=l(()=>`
 <ToggleSwitch
   id="demo-toggle-switch"
   :checked="checked.value"
