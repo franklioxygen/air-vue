43b23389045dfa4b00169fb5c02e45fb196a5fe9
franklioxygen <frank.li.oxygen@gmail.com>
2022-04-17 15:07:15 -0400

    deploy

---
+++ assets/ProjectIntro.bf58cb3e.js
@@ -0,0 +1,5 @@
+import{_ as i}from"./index.b7942227.js";import{s as a,f as l,o as c,c as u,g as r,F as m,a as e}from"./vendor.54853348.js";const d={name:"CustomizedButtonDemo",setup(){const o=a(()=>"npm install air-vue --save"),t=a(()=>`//main.js
+import "air-vue/dist/style.css";
+//App.vue import a Component
+import { ComponentNameHere } from "air-vue";
+`);return{installGuide:o,usageGuide:t}}},p=e("h3",null,"Introduction",-1),_=e("p",null,"Air Vue is a set of open source components library for Vue3",-1),h=e("p",null,"It's light weight and implemented with Composition API",-1),g=e("h5",null,"Setup:",-1),f=e("h5",null,"Usage:",-1);function v(o,t,j,n,C,B){const s=l("highlightjs");return c(),u(m,null,[p,_,h,g,r(s,{language:"cmd",code:n.installGuide},null,8,["code"]),f,r(s,{language:"js",code:n.usageGuide},null,8,["code"])],64)}var V=i(d,[["render",v]]);export{V as default};
---
+++ assets/ToggleSwitchDemo.ff11b522.js
@@ -0,0 +1,23 @@
+import{_ as d,T as _}from"./index.b7942227.js";/* empty css                                                                 */import{s as l,r as h,f as u,o as p,c as m,a as t,g as n,t as v,F as w}from"./vendor.54853348.js";const S={name:"ToggleSwitchDemo",components:{ToggleSwitch:_},setup(){const c=l(()=>`
+<ToggleSwitch
+  id="demo-toggle-switch"
+  :checked="checked.value"
+  :toggle-props="toggleProps"
+  @toggle-switch="toggleButtonEvent"
+/>
+      `),s=l(()=>`
+import { ToggleSwitch } from "air-vue";
+      `),r=l(()=>`
+const checked = reactive({
+    value: false,
+});
+const toggleProps = computed(() => {
+    return {
+        label: "Toggle Switch",
+    };
+});
+const toggleSwitchEvent = (event) => {
+    // Do something with the event
+    toggleEventReturnValue.returnValue = event;
+};
+      `),e=h({value:!1});let g=h({returnValue:e.value});const a=l(()=>({label:"Toggle Switch"}));return{toggleEventReturnValue:g,checked:e,toggleProps:a,insertCode:c,importCode:s,templateCode:r,toggleSwitchEvent:o=>{g.returnValue=o}}}},T=t("h3",null,"Toggle Switch",-1),f=t("p",null,"Toggle Switch is used to toggle.",-1),k=t("h5",null,"Demo:",-1),E={class:"demo-wrapper"},V=t("h5",null,"Usage:",-1),C=t("h6",null,"Insert component:",-1),D=t("h6",null,"Import:",-1),j=t("h6",null,"Apply in template\uFF1A",-1);function x(c,s,r,e,g,a){const i=u("ToggleSwitch"),o=u("highlightjs");return p(),m(w,null,[T,f,k,t("div",E,[n(i,{id:"demo-toggle-switch",checked:e.checked.value,"toggle-props":e.toggleProps,onToggleSwitch:e.toggleSwitchEvent},null,8,["checked","toggle-props","onToggleSwitch"]),t("div",null," This is the value return from Toggle: "+v(e.toggleEventReturnValue.returnValue),1)]),V,C,n(o,{language:"html",code:e.insertCode},null,8,["code"]),D,n(o,{language:"js",code:e.importCode},null,8,["code"]),j,n(o,{language:"js",code:e.templateCode},null,8,["code"])],64)}var R=d(S,[["render",x]]);export{R as default};
