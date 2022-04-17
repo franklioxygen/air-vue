3a15eb451f1a15097e26eb2100b78977a695d2ae
franklioxygen <frank.li.oxygen@gmail.com>
2022-04-17 12:50:24 -0400

    test publish npm

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,21 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       // scss: { additionalData: `@import "./src/assets/_variables.scss";` },
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "./src/index.js"),
+      name: "air-vue",
+      fileName: (format) => `build.${format}.ts`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
     },
   },
 });
