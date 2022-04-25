edf2fb5fa07bb5165e00c38250c7748bdc999125
franklioxygen <frank.li.oxygen@gmail.com>
2022-04-25 18:33:42 -0400

    deploy

---
+++ assets/SlideBarDemo.4084c554.js
@@ -0,0 +1,26 @@
+import{_ as q}from"./index.80fe6262.js";import{i as y}from"./deviceType.f861e0a5.js";import{s as o,r as f,d as w,o as h,c as v,a as t,y as b,C,t as B,e as E,f as V,g as m,F as D}from"./vendor.54853348.js";const T={name:"SlideBar",props:{id:{type:String,required:!0},defaultSlideValue:{type:Number,default:0,required:!0},maxSlideValue:{type:Number,default:100,required:!1},rangeType:{type:String,default:"percentage",required:!1},textPosition:{type:String,default:"float-text"}},emits:["slide-change"],setup(e,{emit:u}){const n=o(()=>e.defaultSlideValue!==null?`width: ${e.defaultSlideValue}%;`:""),l=o(()=>e.defaultSlideValue!==null?`left:${e.defaultSlideValue}% `:"");let d=f({number:e.rangeType==="percentage"?e.defaultSlideValue+"%":e.defaultSlideValue+"/"+e.maxSlideValue});const a=o(()=>document.querySelector("#"+e.id));let s=f({value:!1}),r=f({value:0}),S=i=>{const L=y?i.targetTouches[0].pageX-i.target.getBoundingClientRect().left:null;let c=y?L:i.offsetX;if(s.value&&c>=0&&c<=r.value){a.value.children[0].style.left=c+"px",a.value.children[1].style.width=c+"px";let _=Math.round(c/r.value*e.maxSlideValue);d.number=e.rangeType==="percentage"?_+"%":_+"/"+e.maxSlideValue,u("slide-change",{id:e.id,number:Math.round(_)})}};const x=i=>(r.value=a.value.offsetWidth,s.value=!0,document.querySelector("#app").classList.add("disable-select"),S(i),!1),p=i=>{i.preventDefault(),S(i)},g=()=>{document.querySelector("#app").classList.remove("disable-select"),s.value=!1};return w(()=>{a.value.addEventListener("mousedown",x),a.value.addEventListener("touchstart",x),a.value.parentElement.parentElement.addEventListener("mousemove",p),a.value.parentElement.parentElement.addEventListener("touchmove",p),document.addEventListener("mouseleave",g),document.addEventListener("mouseup",g),document.addEventListener("touchend",g)}),{currentValue:d,liquidStyle:n,ballStyle:l}}},M={class:"slide-wrapper"},N=["id"];function P(e,u,n,l,d,a){return h(),v("div",M,[t("div",{class:"tube",id:n.id},[t("span",{class:"ball",style:b(l.ballStyle)},[n.textPosition==="float-text"?(h(),v("div",{key:0,class:C(["text-percentage",n.textPosition])},B(l.currentValue.number),3)):E("",!0)],4),t("div",{class:"liquid",style:b(l.liquidStyle)},null,4)],8,N),n.textPosition==="fixed-text"?(h(),v("div",{key:0,class:C(["text-percentage",n.textPosition])},B(l.currentValue.number),3)):E("",!0)])}var j=q(T,[["render",P],["__scopeId","data-v-3c949172"]]);const k={name:"SlideBarDemo",components:{SlideBar:j},setup(){const e=o(()=>`
+<!-- normal slide bar -->
+<SlideBar
+  id="range-slider-1"
+  :default-slide-value="80"
+  text-position="float-text"
+  @slide-change="slideChangeEvent"
+/>
+<!-- fixed text slide bar  with custom number-->
+<SlideBar
+  id="range-slider-2"
+  :default-slide-value="0"
+  :max-slide-value="256"
+  range-type="number"
+  text-position="fixed-text"
+  @slide-change="slideChangeEvent"
+/>
+      `),u=o(()=>`
+import { SlideBar } from "air-vue"";
+      `),n=o(()=>`
+const slideChangeEvent = (event) => {
+  // Do something with the event
+  document.querySelector("#" + event.id + "-value").innerHTML =
+    event.number;
+};
+      `);return{insertCode:e,importCode:u,templateCode:n,slideChangeEvent:d=>{document.querySelector("#"+d.id+"-value").innerHTML=d.number}}}},X=t("h3",null,"Slide Bar",-1),I=t("p",null,"Slide Bar is used to slide value.",-1),z=t("h5",null,"Demo:",-1),F={class:"demo-wrapper"},H=t("p",null,"This is a normal Slide Bar with default percentage:",-1),W=t("span",{id:"range-slider-1-value"},null,-1),A=t("p",null,"This is a fixed text Slide Bar with custom number:",-1),O=t("span",{id:"range-slider-2-value"},null,-1),R=t("h5",null,"Usage:",-1),U=t("h6",null,"Insert component:",-1),G=t("h6",null,"Import:",-1),J=t("h6",null,"Apply in template\uFF1A",-1);function K(e,u,n,l,d,a){const s=V("SlideBar"),r=V("highlightjs");return h(),v(D,null,[X,I,z,t("div",F,[H,W,m(s,{id:"range-slider-1","default-slide-value":80,"text-position":"float-text",onSlideChange:l.slideChangeEvent},null,8,["onSlideChange"]),A,O,m(s,{id:"range-slider-2","default-slide-value":0,"max-slide-value":256,"range-type":"number","text-position":"fixed-text",onSlideChange:l.slideChangeEvent},null,8,["onSlideChange"])]),R,U,m(r,{language:"html",code:l.insertCode},null,8,["code"]),G,m(r,{language:"js",code:l.importCode},null,8,["code"]),J,m(r,{language:"js",code:l.templateCode},null,8,["code"])],64)}var $=q(k,[["render",K]]);export{$ as default};
---
+++ assets/TabViewDemo.824a0b6b.js
@@ -0,0 +1,64 @@
+import{_ as v,a as u}from"./index.80fe6262.js";import"./deviceType.f861e0a5.js";import{s as d,r as p,d as y,o as b,c as m,a,F as g,h as w,C,y as P,t as V,D as f,p as k,b as D,E as T,G as _,f as h,g as r,w as x,i as S,H as I}from"./vendor.54853348.js";const q={name:"TabView",props:{id:{type:String,required:!0},isDynamic:{type:Boolean,default:!1},tabsProps:{type:Object,default:()=>({})}},emits:["change-tab"],setup(t,{emit:l}){let o=d(()=>`width: ${100/t.tabsProps.tabs.length}%;`),e=p({index:t.tabsProps.activeTab}),s=(c,i)=>{l("change-tab",{id:t.id,index:c}),e.index=c,t.isDynamic||(document.querySelector(`#${i} .av-tab-content .av-tab-plain-content`).innerHTML=t.tabsProps.tabs[c].content)};return y(()=>{s(e.index,t.id)}),{tabTitleStyle:o,currentActiveTab:e,changeTab:s}}},A=t=>(k("data-v-7734a51a"),t=t(),D(),t),E=["id"],j={class:"av-tabs"},B=["onClick"],L={class:"av-tab-content"},N=A(()=>a("div",{class:"av-tab-plain-content"},null,-1));function R(t,l,o,e,s,c){return b(),m("div",{class:"tab-view-wrapper",id:o.id},[a("ul",j,[(b(!0),m(g,null,w(o.tabsProps.tabs,(i,n)=>(b(),m("li",{class:C(["av-tab-title",{active:n===e.currentActiveTab.index}]),key:n,style:P(e.tabTitleStyle),onClick:Y=>e.changeTab(n,o.id)},V(i.title),15,B))),128))]),a("div",L,[N,a("div",null,[f(t.$slots,"customComponent",{},void 0,!0)])])],8,E)}var O=v(q,[["render",R],["__scopeId","data-v-7734a51a"]]);const U={name:"TabViewDemo",components:{TabView:O},setup(){const t=d(()=>`
+<!-- normal tab view-->
+<p>This is a normal Tab View:</p>
+<TabView
+  id="demo-tab-view-1"
+  :tabs-props="demo1TabsProps"
+  @change-tab="tab1Change"
+>
+</TabView>
+<!-- dynamic tab view-->
+<p>This is a dynamic component Tab View:</p>
+<TabView
+  id="demo-tab-view-2"
+  :isDynamic="true"
+  :tabs-props="demo2TabsProps"
+  @change-tab="tab2Change"
+>
+  <template #customComponent>
+    <component
+      :is="demo2TabsProps.tabs[demo2TabsProps.activeTab].content"
+      :key="demo2TabsProps.tabs[demo2TabsProps.activeTab].key"
+    />
+  </template>
+</TabView>
+      `),l=d(()=>`
+import { TabView } from "air-vue";
+      `),o=d(()=>`
+let demo1TabsProps = reactive({
+  activeTab: 0, // default active tab
+  tabs: [
+    {
+      title: "Tab 1",
+      content: 'Sed malesuada gravida ex, nec tincidunt nunc tempor quis. 
+        Nunc sapien orci, accumsan non mollis congue, scelerisque 
+        at metus. Donec porta euismod convallis. Ut suscipit dolor 
+        risus, sed ultricies tellus pellentesque et.',
+      key: "tab-1",
+    },
+    ...
+  ],
+});
+let demo2TabsProps = reactive({
+  activeTab: 1,  // default active tab
+  tabs: [
+    {
+      title: "Tab 1",
+      content: markRaw(
+        defineAsyncComponent(() => import("./dummy/DynamicLoadDummy-0.vue"))
+      ),
+      key: "tab-1",
+    },
+    ...
+  ],
+});
+const tab1Change = (event) => {
+  demo1TabsProps.activeTab = event.index;
+};
+const tab2Change = (event) => {
+  demo2TabsProps.activeTab = event.index;
+};
+      `);let e=p({activeTab:0,tabs:[{title:"Tab 1",content:`Sed malesuada gravida ex, nec tincidunt nunc tempor quis. 
+            Nunc sapien orci, accumsan non mollis congue, scelerisque 
+            at metus. Donec porta euismod convallis. Ut suscipit dolor 
+            risus, sed ultricies tellus pellentesque et.`,key:"tab-1"},{title:"Tab 2",content:"Tab-2-content",key:"tab-2"},{title:"Tab 3",content:"Tab-3-content",key:"tab-3"}]}),s=p({activeTab:1,tabs:[{title:"Tab 1",content:T(_(()=>u(()=>import("./DynamicLoadDummy-0.7069b65a.js"),["assets/DynamicLoadDummy-0.7069b65a.js","assets/index.80fe6262.js","assets/index.032e4782.css","assets/vendor.54853348.js"]))),key:"tab-1"},{title:"Tab 2",content:T(_(()=>u(()=>import("./DynamicLoadDummy-1.0a0a522e.js"),["assets/DynamicLoadDummy-1.0a0a522e.js","assets/index.80fe6262.js","assets/index.032e4782.css","assets/vendor.54853348.js"]))),key:"tab-2"}]});return{insertCode:t,importCode:l,templateCode:o,demo1TabsProps:e,demo2TabsProps:s,tab1Change:n=>{e.activeTab=n.index},tab2Change:n=>{s.activeTab=n.index}}}},z=a("h3",null,"Tab View",-1),F=a("p",null," Tab View display tabs of content. It can load asynchronous and with multi screens. ",-1),H=a("h5",null,"Demo:",-1),M={class:"demo-wrapper"},$=a("p",null,"This is a normal Tab View:",-1),G=a("p",null,"This is a dynamic component Tab View:",-1),J=a("h5",null,"Usage:",-1),K=a("h6",null,"Insert component:",-1),Q=a("h6",null,"Import:",-1),W=a("h6",null,"Apply in template\uFF1A",-1);function X(t,l,o,e,s,c){const i=h("TabView"),n=h("highlightjs");return b(),m(g,null,[z,F,H,a("div",M,[$,r(i,{id:"demo-tab-view-1","tabs-props":e.demo1TabsProps,onChangeTab:e.tab1Change},null,8,["tabs-props","onChangeTab"]),G,r(i,{id:"demo-tab-view-2",isDynamic:!0,"tabs-props":e.demo2TabsProps,onChangeTab:e.tab2Change},{customComponent:x(()=>[(b(),S(I(e.demo2TabsProps.tabs[e.demo2TabsProps.activeTab].content),{key:e.demo2TabsProps.tabs[e.demo2TabsProps.activeTab].key}))]),_:1},8,["tabs-props","onChangeTab"])]),J,K,r(n,{language:"html",code:e.insertCode},null,8,["code"]),Q,r(n,{language:"js",code:e.importCode},null,8,["code"]),W,r(n,{language:"js",code:e.templateCode},null,8,["code"])],64)}var ae=v(U,[["render",X]]);export{ae as default};
